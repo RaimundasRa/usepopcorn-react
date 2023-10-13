b834b4d1cb0b825b230c4f47fc2f6882fc3e6e6a
Raimundas Radavicius <raimundas_radavicius@yahoo.com>
2023-10-13 11:54:15 +0100

    fetching data & setting up loader

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-import StarRating from "./StarRating.js";
+import "./index.css";
+import App from "./App";
+// import StarRating from "./StarRating.js";
 
 // if another component uses StarRating component and needs certain state from it
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
 
-  return (
-    <div>
-      <StarRating color="green" maxRating={8} onSetRating={setMovieRating} />
-      <p>This movie was rated {movieRating} stars</p>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <StarRating color="green" maxRating={8} onSetRating={setMovieRating} />
+//       <p>This movie was rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating />
+    <App />
+    {/* <StarRating />
     <StarRating defaultRating={3} />
     <StarRating
       size={24}
@@ -28,6 +28,6 @@ root.render(
       messages={["Terrible :(", "Bad", "Okay :|", "Good", "Amazing!"]}
     />
     <StarRating size={30} color="blue" maxRating={10} className="test" />
-    <Test />
+    <Test /> */}
   </React.StrictMode>
 );
